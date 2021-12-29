fda1e4da3ba4825fafb7f45cc252c100bb364a8e
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-12-28 19:28:28 -0600

    Added components dir, TopNav component

---
+++ components/topNav.tsx
@@ -0,0 +1,5 @@
+const TopNav = () => {
+  return <div>Top Nav</div>;
+};
+
+export default TopNav;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,10 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import TopNav from "../components/topNav";
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      <title>Breeze Lakes Point</title>
-      <p>Placeholder for content</p>
-    </div>
-  );
+  return <TopNav></TopNav>;
 };
 
 export default Home;
