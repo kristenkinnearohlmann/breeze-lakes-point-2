e82531dd1d31f76a3d9676477aaa7c0fdf2b2bb1
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-12-28 19:49:46 -0600

    Added dimensions and colors to nav elements for placement

--- components/appLayout.tsx
+++ components/appLayout.tsx
@@ -1,12 +1,18 @@
+import { Box } from "@chakra-ui/layout";
 import TopNav from "./topNav";
 import LeftNav from "./leftNav";
 
-const AppLayout = ({ children }) => {
+const AppLayout = ({ children }: { children: any }) => {
   return (
-    <div>
-      <TopNav></TopNav>
-      <LeftNav></LeftNav>
-    </div>
+    <Box>
+      <Box height="100px" width="100vw" bg="green">
+        <TopNav></TopNav>
+      </Box>
+      <Box height="calc(100vh - 100px)" width="100px" bg="purple">
+        <LeftNav></LeftNav>
+      </Box>
+      <Box>{children}</Box>
+    </Box>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,9 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import AppLayout from "../components/appLayout";
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      <AppLayout></AppLayout>
-    </div>
-  );
+  return <div>Hello world</div>;
 };
 
 export default Home;
