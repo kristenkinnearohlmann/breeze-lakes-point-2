1501168087b84e3c3b763b62cd98bca496968ef2
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-12-31 16:46:05 -0600

    Added placeholder for form section

--- components/authForm.tsx
+++ components/authForm.tsx
@@ -7,12 +7,15 @@ const AuthForm = () => {
       <Flex
         justify="center"
         align="center"
-        height="214px"
+        height="215px"
         borderBottom="white 1px solid"
         paddingX="10px"
       >
         <NextImage src="/logo.png" height={194} width={160} />
         <Text fontSize="3xl">Breeze Lakes Point</Text>
+      </Flex>
+      <Flex justify="center" align="center" height="calc(100vh - 215px)">
+        <Text>ph</Text>
       </Flex>
     </Box>
   );
