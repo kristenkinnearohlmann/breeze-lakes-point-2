c6e316e34d43e016eb799d32cea18758b5becbee
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-12-31 16:34:01 -0600

    Fixed imports in signup and signin, started form design for AuthForm

--- components/authForm.tsx
+++ components/authForm.tsx
@@ -1,5 +1,17 @@
+import { Box, Flex, Input, Button } from "@chakra-ui/react";
+import NextImage from "next/image";
+
 const AuthForm = () => {
-  return <div>Auth Form</div>;
+  return (
+    <Box height="100vh" width="100vw" bg="grey" color="white">
+      <Flex
+        justify="center"
+        align="center"
+        height="100px"
+        borderBottom="white 1px solid"
+      ></Flex>
+    </Box>
+  );
 };
 
 export default AuthForm;
