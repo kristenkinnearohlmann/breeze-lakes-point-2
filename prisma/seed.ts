0e18db0472dc8635a01561394550a1ba0f06ba8a
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2022-01-01 20:49:48 -0600

    Completed seed file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,4 +5,24 @@ const prisma = new PrismaClient();
 
 const run = async () => {
   const salt = bcrypt.genSaltSync();
+  const user = await prisma.user.upsert({
+    where: { username: "user@test.com" },
+    update: {},
+    create: {
+      username: "user@test.com",
+      password: bcrypt.hashSync("password", salt),
+      email: "user@test.com",
+      firstName: "Test",
+      lastName: "User",
+    },
+  });
 };
+
+run()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
