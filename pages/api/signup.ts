f4f3ef9742df6dce54d5d4922b608a6fc9740cbc
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2022-01-01 21:28:07 -0600

    Finished signup API

--- pages/api/signup.ts
+++ pages/api/signup.ts
@@ -4,7 +4,52 @@ import cookie from "cookie";
 import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../lib/prisma";
 
-export default async () => {};
+export default async (
+  req: NextApiRequest,
+  res: NextApiResponse,
+  { appSecret }: { appSecret: string }
+) => {
+  const salt = bcrypt.genSaltSync();
+  const { username, password } = req.body;
+
+  let user;
+
+  try {
+    user = await prisma.user.create({
+      data: {
+        username,
+        password: bcrypt.hashSync(password, salt),
+      },
+    });
+  } catch (e) {
+    res.status(401);
+    res.json({ error: "User already exists" });
+    return;
+  }
+
+  const token = jwt.sign(
+    {
+      username: user.username,
+      id: user.id,
+      time: Date.now(),
+    },
+    appSecret,
+    { expiresIn: "8h" }
+  );
+
+  res.setHeader(
+    "Set-Cookie",
+    cookie.serialize("BREEZE_LAKES_POINT_ACCESS_TOKEN", token, {
+      httpOnly: true,
+      maxAge: 8 * 60 * 60,
+      path: "/",
+      sameSite: "lax",
+      secure: process.env.NODE_ENV === "production",
+    })
+  );
+
+  res.json(user);
+};
 
 export const getServerSideProps = async () => {
   const appSecret = process.env.BREEZE_LAKES_POINT_SECRET;
