c4f51ff24af918c14334b7a352c32d91adc9e67c
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2022-01-01 21:17:08 -0600

    Completed signin API

--- pages/api/signin.ts
+++ pages/api/signin.ts
@@ -1,3 +1,59 @@
 import bycrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import cookie from "cookie";
+import prisma from "../../lib/prisma";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async (
+  req: NextApiRequest,
+  res: NextApiResponse,
+  { appSecret }: { appSecret: string }
+) => {
+  const { username, password } = req.body;
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username: username,
+    },
+  });
+
+  if (user && bycrypt.compareSync(password, user.password)) {
+    const token = jwt.sign(
+      {
+        id: user.id,
+        username: user.username,
+        time: Date.now(),
+      },
+      appSecret,
+      {
+        expiresIn: "8h",
+      }
+    );
+
+    res.setHeader(
+      "Set-Cookie",
+      cookie.serialize("BREEZE_LAKES_POINT_ACCESS_TOKEN", token, {
+        httpOnly: true,
+        maxAge: 8 * 60 * 60,
+        path: "/",
+        sameSite: "lax",
+        secure: process.env.NODE_ENV === "production",
+      })
+    );
+
+    res.json(user);
+  } else {
+    res.status(401);
+    res.json({ error: "Username or Password is incorrect" });
+  }
+};
+
+export const getServerSideProps = async () => {
+  const appSecret = process.env.BREEZE_LAKES_POINT_SECRET;
+
+  return {
+    props: {
+      appSecret,
+    },
+  };
+};
