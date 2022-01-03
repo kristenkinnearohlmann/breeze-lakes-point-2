75587362da2c966328e5e398a9446be902533a93
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2022-01-03 17:59:07 -0600

    Working through signup API

--- pages/api/signup.ts
+++ pages/api/signup.ts
@@ -4,6 +4,11 @@ import cookie from "cookie";
 import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../lib/prisma";
 
+type Data = {
+  username: string;
+  password: string;
+};
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const appSecret: any = process.env.BREEZE_LAKES_POINT_SECRET;
   const salt = bcrypt.genSaltSync();
@@ -11,39 +16,47 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
   let user;
 
-  try {
-    user = await prisma.user.create({
-      data: {
-        username,
-        password: bcrypt.hashSync(password, salt),
-      },
-    });
-  } catch (e) {
-    res.status(401);
-    res.json({ error: "User already exists" });
-    return;
-  }
-
-  const token = jwt.sign(
-    {
-      username: user.username,
-      id: user.id,
-      time: Date.now(),
-    },
-    appSecret,
-    { expiresIn: "8h" }
-  );
-
-  res.setHeader(
-    "Set-Cookie",
-    cookie.serialize("BREEZE_LAKES_POINT_ACCESS_TOKEN", token, {
-      httpOnly: true,
-      maxAge: 8 * 60 * 60,
-      path: "/",
-      sameSite: "lax",
-      secure: process.env.NODE_ENV === "production",
-    })
-  );
-
-  res.json(user);
+  res.json({
+    msg: "Completed signup route",
+    appSecret: appSecret,
+    user: username,
+    pass: password,
+  });
 };
+
+//   try {
+//     user = await prisma.user.create({
+//       data: {
+//         username,
+//         password: bcrypt.hashSync(password, salt),
+//       },
+//     });
+//   } catch (e) {
+//     res.status(401);
+//     res.json({ error: "User already exists" });
+//     return;
+//   }
+
+//   const token = jwt.sign(
+//     {
+//       username: user.username,
+//       id: user.id,
+//       time: Date.now(),
+//     },
+//     appSecret,
+//     { expiresIn: "8h" }
+//   );
+
+//   res.setHeader(
+//     "Set-Cookie",
+//     cookie.serialize("BREEZE_LAKES_POINT_ACCESS_TOKEN", token, {
+//       httpOnly: true,
+//       maxAge: 8 * 60 * 60,
+//       path: "/",
+//       sameSite: "lax",
+//       secure: process.env.NODE_ENV === "production",
+//     })
+//   );
+
+//   res.json(user);
+// };
