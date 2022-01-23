75d13332bf697bbe5edd34621ba9e39e544bc117
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2022-01-22 19:26:17 -0600

    Completed roleData information for seeding

--- prisma/roleData.ts
+++ prisma/roleData.ts
@@ -1,1 +1,23 @@
-export const roleData: {};
+export const roleData: {
+  roleName: string;
+  description: string;
+}[] = [
+  {
+    roleName: "User",
+    description: "Can add and modify own information",
+  },
+  {
+    roleName: "Admin",
+    description:
+      "Can add and modify own information, can add and modify limited information for other users",
+  },
+  {
+    roleName: "Manager",
+    description:
+      "Can add and modify own information, can add and modify all information for other users, can set roles for users",
+  },
+  {
+    roleName: "SuperAdmin",
+    description: "Can manage entire application",
+  },
+];
