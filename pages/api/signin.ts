25e0ac9a3cb356203ab2ac71ce795b50a565754e
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2022-01-28 12:51:13 -0600

    Added console log for remote troubleshooting

--- pages/api/signin.ts
+++ pages/api/signin.ts
@@ -14,6 +14,8 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     },
   });
 
+  console.log(user);
+
   if (user && bcrypt.compareSync(password, user.password)) {
     const token = jwt.sign(
       {
