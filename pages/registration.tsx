76c285f3bdeac4144e2bcf6765a46443d2e7fae6
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2022-02-04 20:58:50 -0600

    Updated First Name field using Formik

--- pages/registration.tsx
+++ pages/registration.tsx
@@ -1,12 +1,32 @@
 import { Box, Text } from "@chakra-ui/layout";
 import { Input } from "@chakra-ui/react";
+import { Formik } from "formik";
 import { FormControl, FormLabel } from "@chakra-ui/form-control";
+import * as Yup from "yup";
 import UserHeader from "../components/userHeader";
 import { useMe } from "../lib/hooks";
+import { EventEmitter } from "stream";
+import { eventNames } from "process";
+import { InputControl } from "formik-chakra-ui";
 
 const Registration = () => {
   const { user } = useMe();
   console.log(user);
+
+  const initialValues = {
+    firstName: user.firstName,
+  };
+
+  console.log(initialValues);
+  const validationSchema = Yup.object({
+    firstName: Yup.string().required(),
+  });
+
+  const onSubmit = (values) => {
+    event.preventDefault;
+    console.log(values);
+  };
+
   return (
     <div>
       <UserHeader user={user}></UserHeader>
@@ -14,16 +34,29 @@ const Registration = () => {
         <Text>Complete or review your registration information.</Text>
       </Box>
       <Box>
-        <form>
-          <FormControl isRequired>
-            <FormLabel htmlFor="first-name">First name</FormLabel>
-            <Input
-              id="first-name"
-              name="first-name"
-              placeholder="First name"
-            ></Input>
-          </FormControl>
-        </form>
+        <Formik
+          initialValues={initialValues}
+          onSubmit={onSubmit}
+          validationSchema={validationSchema}
+        >
+          {({ handleSubmit, values, errors }) => (
+            // <FormControl isRequired>
+            //   <FormLabel htmlFor="first-name">First name</FormLabel>
+            //   <Input
+            //     id="first-name"
+            //     name="first-name"
+            //     placeholder="First name"
+            //   ></Input>
+            // </FormControl>
+            <InputControl
+              isRequired
+              isDisabled
+              id="firstName"
+              name="firstName"
+              label="First name"
+            />
+          )}
+        </Formik>
       </Box>
     </div>
   );
