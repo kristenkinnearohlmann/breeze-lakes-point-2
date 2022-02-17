f7d92d4d2fdb89e3dae2e32fbea5ed468125f261
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2022-02-16 19:55:31 -0600

    Added basic setup to Admin page

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,5 +1,19 @@
+import { Box, Text } from "@chakra-ui/layout";
+import HeadMeta from "./partials/headMeta";
+import { useMe } from "../lib/hooks";
+
 const Admin = () => {
-  return <div>Admin page</div>;
+  const { user } = useMe();
+  return (
+    <div>
+      <HeadMeta subtitle="Admin" />
+      <Box>
+        <Text borderBottom="2px solid #fff" fontSize="3xl" marginBottom="15px">
+          Admin
+        </Text>
+      </Box>
+    </div>
+  );
 };
 
 export default Admin;
