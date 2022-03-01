7dffc52848adc52a53138f2b0218ded6fb325b25
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2022-02-28 22:46:59 -0600

    Retrieved initial query parm for userId

--- pages/api/users/[userId].ts
+++ pages/api/users/[userId].ts
@@ -1,6 +1,12 @@
 import { getUser } from "../../../lib/data";
+import { NextApiRequest, NextApiResponse } from "next";
 
-export default getUser(async (req, res, data) => {
-  console.log("GET single user");
-  res.json(data);
-});
+export default getUser(
+  async (req: NextApiRequest, res: NextApiResponse, data) => {
+    console.log("GET single user");
+    console.log(data);
+    console.log(req.query.userId);
+    const userId = req.query.userId;
+    res.json(data);
+  }
+);
