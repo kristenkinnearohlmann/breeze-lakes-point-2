fcdbeddbb093ba1756e72fbf6874e393fa7dc2bd
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2022-03-02 21:24:28 -0600

    Added analysis of flow to fix data api work

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -20,3 +20,9 @@ export const useUser = (dataUserId) => {
     isError: error,
   };
 };
+
+// hooks: useMe calls /me route
+
+// /me: is an api route that imports validateRoute from lib auth, passes args to it and returning result
+
+// auth: a file in lib that defines the validateRoute function as a  closure and does the database read
