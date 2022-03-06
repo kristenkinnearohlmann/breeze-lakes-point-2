652e04e2541e03b2ad1e4a4a8f618204e1ed34a2
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2022-03-05 19:52:43 -0600

    Cleaned up console logs in data lib

--- lib/data.ts
+++ lib/data.ts
@@ -9,11 +9,8 @@ export const getUsers = (handler) => {
 
 export const getUser = (handler) => {
   return async (req: NextApiRequest, res: NextApiResponse) => {
-    console.log("Unpack handler?");
-    const { data } = handler;
-    console.log("Inside data lib", data);
     console.log("Is request query here", req.query);
     const val = { label: "Single user" };
-    return handler(req, res, data);
+    return handler(req, res, val);
   };
 };
