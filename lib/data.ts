cd79df6e8850ba8cf8abfac53eb9477bd9c09fca
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2022-03-08 22:55:22 -0600

    Added console log to track getUsers from data

--- lib/data.ts
+++ lib/data.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export const getUsers = (handler) => {
   return async (req: NextApiRequest, res: NextApiResponse) => {
+    console.log("HTTP verb: ", req.method);
     const data = { label: "All users" };
     return handler(req, res, data);
   };
