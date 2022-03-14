f2e8cfb572f94e468042aefdab690dde4c4c5432
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2022-03-14 18:15:38 -0500

    Started work on retrieving user data for registration

--- lib/data.ts
+++ lib/data.ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "prisma";
 
 export const getUsers = (handler) => {
   return async (req: NextApiRequest, res: NextApiResponse) => {
@@ -12,13 +13,28 @@ export const getUsers = (handler) => {
 export const handleUserData = (handler) => {
   return async (req: NextApiRequest, res: NextApiResponse) => {
     console.log("HTTP verb: ", req.method);
-    console.log(userData());
     const qry = req.query.userId;
+    console.log(userData(req.query.userId));
     const data = { label: "Obvious value", userId: qry };
     return handler(req, res, data);
   };
 };
 
-const userData = () => {
-  return { msg: "Return data from userData" };
+const userData = async (qry) => {
+  // return { msg: "Return data from userData" };
+  // Need further work from: https://www.prisma.io/docs/concepts/components/prisma-client/select-fields
+  console.log("In userData function", qry);
+  // if (qry === undefined) return;
+  // const result: object | null = await prisma.user.findUnique({
+  //   where: {
+  //     id: qry,
+  //   },
+  //   select: {
+  //     username: true,
+  //     email: true,
+  //     firstName: true,
+  //     lastName: true,
+  //   },
+  // });
+  // return result;
 };
