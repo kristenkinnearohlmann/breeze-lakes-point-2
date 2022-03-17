fa78e2b1f5bf6f031fcc2c2560d20cd85940f1d2
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2022-03-16 21:45:00 -0500

    Confirmed return value reaches frontend for user data

--- lib/data.ts
+++ lib/data.ts
@@ -13,19 +13,12 @@ export const getUsers = (handler) => {
 export const handleUserData = (handler) => {
   return async (req: NextApiRequest, res: NextApiResponse) => {
     console.log("HTTP verb: ", req.method);
-    const qry = req.query.userId;
-    const answer = await userData(req.query.userId);
-    console.log(answer);
-    const data = { label: "Obvious value", userId: qry };
+    const data = await userData(req.query.userId);
     return handler(req, res, data);
   };
 };
 
 const userData = async (qry) => {
-  // return { msg: "Return data from userData" };
-  // Need further work from: https://www.prisma.io/docs/concepts/components/prisma-client/select-fields
-  console.log("In userData function", qry);
-
   if (qry === undefined) return;
 
   const data = await prisma.user.findUnique({
@@ -40,21 +33,5 @@ const userData = async (qry) => {
     },
   });
 
-  const result = { ...data, msg: "Found" };
-  console.log("result is", result);
-  // const result: object | null = await prisma.user.findUnique({
-  //   where: {
-  //     id: qry,
-  //   },
-  //   select: {
-  //     username: true,
-  //     email: true,
-  //     firstName: true,
-  //     lastName: true,
-  //   },
-  // });
-  // return result;
-  console.log(qry);
-
-  return { label: "Stuff happened" };
+  return { ...data, msg: "Found" };
 };
