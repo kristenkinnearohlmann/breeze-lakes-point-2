c8fe4653a987765588c9fe553a511c8638fb4ecd
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2022-04-05 17:09:51 -0500

    Updated style on role

--- components/userHeader.tsx
+++ components/userHeader.tsx
@@ -3,8 +3,8 @@ import { Box, Text, Divider, Flex } from "@chakra-ui/layout";
 const UserHeader = ({ user }: { user: any }) => {
   return (
     <Box padding="0 0 15px 0">
-      <Flex>
-        <Text fontSize="3xl">
+      <Flex align="center">
+        <Text fontSize="3xl" marginRight="15px">
           {user?.firstName} {user?.lastName}
         </Text>
         <Text fontSize="large">{user?.role}</Text>
