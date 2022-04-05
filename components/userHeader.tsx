d327567e86259b6d40454b70d695373554e56207
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2022-04-05 17:07:19 -0500

    Moved role to same row as name for user header for further styling

--- components/userHeader.tsx
+++ components/userHeader.tsx
@@ -1,12 +1,14 @@
-import { Box, Text, Divider } from "@chakra-ui/layout";
+import { Box, Text, Divider, Flex } from "@chakra-ui/layout";
 
 const UserHeader = ({ user }: { user: any }) => {
   return (
     <Box padding="0 0 15px 0">
-      <Text fontSize="3xl">
-        {user?.firstName} {user?.lastName}
-      </Text>
-      <Text fontSize="large">{user?.role}</Text>
+      <Flex>
+        <Text fontSize="3xl">
+          {user?.firstName} {user?.lastName}
+        </Text>
+        <Text fontSize="large">{user?.role}</Text>
+      </Flex>
       <Divider />
     </Box>
   );
