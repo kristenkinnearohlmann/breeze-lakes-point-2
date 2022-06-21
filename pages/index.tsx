4030249ab9d94657ed7a922c402d45bf84c1361b
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2022-06-20 20:43:30 -0500

    Adjust placeholder for sample jsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,11 +10,11 @@ const Home: NextPage = () => {
     <div>
       <UserHeader user={user}></UserHeader>
       <Dashboard user={user}></Dashboard>
-      <p>sample</p>
+      <p>This is a placeholder for a sample JSX declaration.</p>
       <style jsx>{`
         p {
-          background-color: red;
-          font-size: 5em;
+          background-color: blueviolet;
+          font-size: 2.5em;
         }
       `}</style>
     </div>
